00905f1773017093c39a7afd56fa4637cb8d5358
Arpad Olasz <arpi@wingsline.com>
2014-04-10 13:42:20 -0500

    Added support for open multiple tabs with the hidden input element.

--- jquery.tabs.js
+++ jquery.tabs.js
@@ -1,5 +1,5 @@
 /**
- * tabs plugin v0.1.5.1
+ * tabs plugin v0.1.5.2
  * http://wingsline.com
  *
  * Copyright 2012 Arpad Olasz
@@ -21,273 +21,277 @@
  * Required HTML:
  * --------------------------------------------------------------
  *
- *	<ol class="tabs">
- *		<li><a href="#tab1">Tab 1</a></li>
- *		<li><a href="#tab2">Tab 2</a></li>
- *		<li><a href="#tab3">Tab 3</a></li>
- *	</ol>
- *	<ul class="tabs--content">
- *		<li id="tab1">Content 1</li>
- *		<li id="tab2">Content 2</li>
- *		<li id="tab3">Content 3</li>
- *	</ul>
+ *  <ol class="tabs">
+ *      <li><a href="#tab1">Tab 1</a></li>
+ *      <li><a href="#tab2">Tab 2</a></li>
+ *      <li><a href="#tab3">Tab 3</a></li>
+ *  </ol>
+ *  <ul class="tabs--content">
+ *      <li id="tab1">Content 1</li>
+ *      <li id="tab2">Content 2</li>
+ *      <li id="tab3">Content 3</li>
+ *  </ul>
  *
  *
  * Usage:
  * --------------------------------------------------------------
  *
- *	Initialize the plugin:
- *	$('.tabs').tabs();
+ *  Initialize the plugin:
+ *  $('.tabs').tabs();
  *
- *	When initializing, you can pass options to the plugin:
- *	$('.tabs').tabs({'option1':'value', 'option2': 'value'});
+ *  When initializing, you can pass options to the plugin:
+ *  $('.tabs').tabs({'option1':'value', 'option2': 'value'});
  *
  *
- *	The following methods will open a tab:
+ *  The following methods will open a tab:
  *
- *	1. HTML
- *	Insert a hidden input element into the HTML:
- *	<input name="openTab" value="tab1" type="hidden"/>
+ *  1. HTML
+ *  Insert a hidden input element into the HTML:
+ *  <input name="openTab" value="tab1" type="hidden"/>
  *
- *	2. Javascript
- *	Add this after the plugin was initialized:
- *	$('.tabs').tabs('open', 'tabname');
+ *  2. Javascript
+ *  Add this after the plugin was initialized:
+ *  $('.tabs').tabs('open', 'tabname');
  *
- *	If you want to set the hash of the current URL:
- *	$('.tabs').tabs('open', 'langhu', true);
+ *  If you want to set the hash of the current URL:
+ *  $('.tabs').tabs('open', 'langhu', true);
  *
- *	3. Open the first or the last tab
- *	$('.tabs').tabs('open', 'first');
- *	$('.tabs').tabs('open', 'last');
+ *  3. Open the first or the last tab
+ *  $('.tabs').tabs('open', 'first');
+ *  $('.tabs').tabs('open', 'last');
  *
  *
  * Notes:
  * --------------------------------------------------------------
  *
- *	- the tab content must be inserted after the tab handlers
- *	- the plugin adds the class "active" to the li elements, so make
- *	sure in your style sheet you hide the li elemenets
- *	and show the li.active ones
+ *  - the tab content must be inserted after the tab handlers
+ *  - the plugin adds the class "active" to the li elements, so make
+ *  sure in your style sheet you hide the li elemenets
+ *  and show the li.active ones
  *
  */
 
 (function( $ ){
-	'use strict';
-		// namespace for events and data
-		// use it like 'click.' + __NS__ for the event
-		// and data(__NS__) for data
-	var __NS__ = 'tabs',
-		// public methods
-		methods = {
+    'use strict';
+        // namespace for events and data
+        // use it like 'click.' + __NS__ for the event
+        // and data(__NS__) for data
+    var __NS__ = 'tabs',
+        // public methods
+        methods = {
 
-			/**
-			 * Initializes the plugin
-			 * @param  {object} options Options object
-			 * @return {object}         Tabs object
-			 */
-			init : function( options ) {
+            /**
+             * Initializes the plugin
+             * @param  {object} options Options object
+             * @return {object}         Tabs object
+             */
+            init : function( options ) {
 
-				return this.each(function(){
+                return this.each(function(){
 
-					var $this = $(this),
-						data = $this.data(__NS__);
+                    var $this = $(this),
+                        data = $this.data(__NS__);
 
-					// If the plugin hasn't been initialized yet
-					if ( ! data ) {
-						// default options
-						data = $.extend( {
-							// the class for the tabbed content container
-							'tabContent' : '.tabs--content',
-							// content element in the container will have this class when open
-							'activeClass': 'active',
-							// if a hidden element is present in the page we attempt to open that tab
-							'hiddenInputName': 'openTab'
-						}, options);
+                    // If the plugin hasn't been initialized yet
+                    if ( ! data ) {
+                        // default options
+                        data = $.extend( {
+                            // the class for the tabbed content container
+                            'tabContent' : '.tabs--content',
+                            // content element in the container will have this class when open
+                            'activeClass': 'active',
+                            // if a hidden element is present in the page we attempt to open that tab
+                            'hiddenInputName': 'openTab'
+                        }, options);
 
-						$(this).data(__NS__, data);
+                        $(this).data(__NS__, data);
 
-						// update the validIds for the tabs
-						methods.update.apply($this);
+                        // update the validIds for the tabs
+                        methods.update.apply($this);
 
-						// if there is a hidden element in the page open that tab
-						var openTab = $(':hidden[name="'+data.hiddenInputName+'"]');
-
-						if(openTab.length)
-						{
-							methods.open.apply($this, [$(openTab).val().toString()]);
-						}
+                        // if there is a hidden element in the page open that tab
+                        var openTab = $(':hidden[name="'+data.hiddenInputName+'"]'),
+                            openTabArray = $(':hidden[name="'+data.hiddenInputName+'[]"]');
 
 
-						// check if the url contains a hashtag that refers to the tab
-						else if(window.location.hash)
-						{
-							var hash = window.location.hash.substr(1);
-							$.extend($this.data(__NS__), {'hash':hash});
-							// attempt to auto-open the tab if it is a valid id
-							if($.inArray(hash, data.validIds) >= 0)
-							{
-								methods.open.apply($this, [hash]);
-							}
+                        // if there are multiple opentab arrays
+                        if (openTabArray.length) {
+                            openTabArray.each(function () {
+                                methods.open.apply($this, [$(this).val().toString()]);
+                            });
+                        } else if(openTab.length) {
+                            methods.open.apply($this, [$(openTab).val().toString()]);
+                        }
 
-						}
+                        else if(window.location.hash)
+                        {
+                            var hash = window.location.hash.substr(1);
+                            $.extend($this.data(__NS__), {'hash':hash});
+                            // attempt to auto-open the tab if it is a valid id
+                            if($.inArray(hash, data.validIds) >= 0)
+                            {
+                                methods.open.apply($this, [hash]);
+                            }
 
-						// add the events to the li > a elements
-						/* jQuery 9 compatibility */
-						$(this).on('click.' + __NS__, 'li > a', function(e)
-						// $('li > a', $this).live('click.' + __NS__, function(e)
-						{
-							// update the valid tabs
-							methods.update.apply($this);
+                        }
 
-							var aHash = this.hash.substr(1);
-							if(aHash)
-							{
-								e.preventDefault();
-								window.location.hash = '#' + aHash;
-								methods.open.apply($this, [aHash, e]);
-							}
-						});
-					}
-				});
-			}, // end init()
+                        // add the events to the li > a elements
+                        /* jQuery 9 compatibility */
+                        $(this).on('click.' + __NS__, 'li > a', function(e)
+                        // $('li > a', $this).live('click.' + __NS__, function(e)
+                        {
+                            // update the valid tabs
+                            methods.update.apply($this);
 
-
-			/**
-			 * This function essentially updates the tab id's in case new tabs are added
-			 * @return {object} Tabs object
-			 */
-			update : function ( ) {
-
-				return this.each(function(){
-
-					var $this = $(this),
-						data = $this.data(__NS__);
-
-					data.validIds = [];
-					$('li > a', $this).each(function()
-					{
-						var aHash = this.hash.substr(1);
-						if(aHash)
-						{
-							data.validIds.push(aHash);
-						}
-					});
-
-				});
-			},
+                            var aHash = this.hash.substr(1);
+                            if(aHash)
+                            {
+                                e.preventDefault();
+                                window.location.hash = '#' + aHash;
+                                methods.open.apply($this, [aHash, e]);
+                            }
+                        });
+                    }
+                });
+            }, // end init()
 
 
-			/**
-			 * Removes every trace of the tabs plugin from the DOM
-			 * @return {object} Tabs object
-			 */
-			destroy : function( ) {
+            /**
+             * This function essentially updates the tab id's in case new tabs are added
+             * @return {object} Tabs object
+             */
+            update : function ( ) {
 
-				return this.each(function(){
+                return this.each(function(){
 
-					var $this = $(this),
-						data = $this.data(__NS__);
+                    var $this = $(this),
+                        data = $this.data(__NS__);
 
-					$(window).unbind('.'+__NS__);
-					/* jQuery 1.9 compatibility */
-					$(document).off('.'+__NS__, 'li > a');
-					// $('li > a').die('.'+__NS__);
-					data[__NS__].remove();
-					$this.removeData(__NS__);
+                    data.validIds = [];
+                    $('li > a', $this).each(function()
+                    {
+                        var aHash = this.hash.substr(1);
+                        if(aHash)
+                        {
+                            data.validIds.push(aHash);
+                        }
+                    });
 
-				});
-
-			}, // end destroy()
-
-
-			/**
-			 * Opens a tab, closing the others
-			 * @param  {string} hash Id of the tab to be opened
-			 * @return {object}      Tabs element
-			 */
-			open: function (hash, e) {
-
-				return this.each(function(){
-
-					var $this = $(this),
-						data = $this.data(__NS__),
-						container = $this.next(data.tabContent);
-
-					// if the requested hash is not in the validIds we don't do nothing
-					if($.inArray(hash, data.validIds) < 0 && hash !== 'first' && hash !== 'last') {
-						return;
-					}
-
-					// if hash == first, we open the 1st tab
-					if(hash === 'first')
-					{
-
-						hash = data.validIds[0];
-					}
-					// if hash == last, we open the last tab
-					else if (hash === 'last')
-					{
-						hash = data.validIds[data.validIds.length - 1];
-					}
+                });
+            },
 
 
-					if(container.length)
-					{
-						// make the tab active
-						// get hash of the A element
-						$this.children('li').each(function (){
-							var a = $('a:first', this);
+            /**
+             * Removes every trace of the tabs plugin from the DOM
+             * @return {object} Tabs object
+             */
+            destroy : function( ) {
 
-							if(a.length)
-							{
+                return this.each(function(){
 
-								var aHash = a.get(0).hash.substr(1);
+                    var $this = $(this),
+                        data = $this.data(__NS__);
 
-								if(aHash)
-								{
-									// if the hash of the a elmenet matches, we make this active
-									if(aHash === hash)
-									{
-										$(this).addClass(data.activeClass);
-										// update the hash only if we have hash
-										if(undefined !== e && window.location.hash)
-										{
-											window.location.hash = '#' + aHash;
-										}
+                    $(window).unbind('.'+__NS__);
+                    /* jQuery 1.9 compatibility */
+                    $(document).off('.'+__NS__, 'li > a');
+                    // $('li > a').die('.'+__NS__);
+                    data[__NS__].remove();
+                    $this.removeData(__NS__);
 
-									}
-									// otherwise we remove the active class from the li
-									else
-									{
-										$(this).removeClass(data.activeClass);
-									}
-								}
+                });
 
-							}
-						});
+            }, // end destroy()
 
 
-						// show the tab content
-						$('#' + hash, container).addClass(data.activeClass);
-						// hide other tab content
-						container.children().not($('#'+hash)).removeClass(data.activeClass);
-					}
+            /**
+             * Opens a tab, closing the others
+             * @param  {string} hash Id of the tab to be opened
+             * @return {object}      Tabs element
+             */
+            open: function (hash, e) {
 
-				});
-			} // end open ()
+                return this.each(function(){
 
-		}; // end methods
+                    var $this = $(this),
+                        data = $this.data(__NS__),
+                        container = $this.next(data.tabContent);
 
-		$.fn.tabs = function( method ) {
+                    // if the requested hash is not in the validIds we don't do nothing
+                    if($.inArray(hash, data.validIds) < 0 && hash !== 'first' && hash !== 'last') {
+                        return;
+                    }
 
-			if ( methods[method] ) {
-				return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
-			} else if ( typeof method === 'object' || ! method ) {
-				return methods.init.apply( this, arguments );
-			} else {
-				$.error( 'Method ' +  method + ' does not exist on jQuery.tabs' );
-			}
+                    // if hash == first, we open the 1st tab
+                    if(hash === 'first')
+                    {
 
-		}; // end plugin
+                        hash = data.validIds[0];
+                    }
+                    // if hash == last, we open the last tab
+                    else if (hash === 'last')
+                    {
+                        hash = data.validIds[data.validIds.length - 1];
+                    }
+
+
+                    if(container.length)
+                    {
+                        // make the tab active
+                        // get hash of the A element
+                        $this.children('li').each(function (){
+                            var a = $('a:first', this);
+
+                            if(a.length)
+                            {
+
+                                var aHash = a.get(0).hash.substr(1);
+
+                                if(aHash)
+                                {
+                                    // if the hash of the a elmenet matches, we make this active
+                                    if(aHash === hash)
+                                    {
+                                        $(this).addClass(data.activeClass);
+                                        // update the hash only if we have hash
+                                        if(undefined !== e && window.location.hash)
+                                        {
+                                            window.location.hash = '#' + aHash;
+                                        }
+
+                                    }
+                                    // otherwise we remove the active class from the li
+                                    else
+                                    {
+                                        $(this).removeClass(data.activeClass);
+                                    }
+                                }
+
+                            }
+                        });
+
+
+                        // show the tab content
+                        $('#' + hash, container).addClass(data.activeClass);
+                        // hide other tab content
+                        container.children().not($('#'+hash)).removeClass(data.activeClass);
+                    }
+
+                });
+            } // end open ()
+
+        }; // end methods
+
+        $.fn.tabs = function( method ) {
+
+            if ( methods[method] ) {
+                return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+            } else if ( typeof method === 'object' || ! method ) {
+                return methods.init.apply( this, arguments );
+            } else {
+                $.error( 'Method ' +  method + ' does not exist on jQuery.tabs' );
+            }
+
+        }; // end plugin
 
 })( jQuery );
